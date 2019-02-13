9d13424a57b7b6e3c299c580240f2e6054bfdd0c
virusmf <44576737+virusmf@users.noreply.github.com>
2019-02-13 22:28:03 +0500

    работаем с классами

--- gbadd.php
+++ gbadd.php
@@ -1,12 +1,14 @@
 <?php
 require_once __DIR__ . '/classes/GuestBook.php';
+
 $fh =__DIR__ . '/guestbook.txt';
+
 $guestbook = new GuestBook($fh);
+
 if ($_POST['TC'] != '' && $_POST['text'] != '') {
     $rec = $_POST['TC'] . '||' . $_POST['text'];
     $guestbook->append($rec);
     $guestbook->save($fh);
-
 }
 
 header('Location: /');
